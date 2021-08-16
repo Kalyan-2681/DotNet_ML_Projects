41d8aa5c8d24fa751e589de4d409948bb9c574c2
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2021-08-15 22:44:10 -0400

    updated code

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -6,15 +6,18 @@ import logging
 app = Flask(__name__)
 api = Api(app)
 
-recommendations = {}
+recommendations = {"001":{"fruits":{"apple", "mango"}, 
+                    "vegetables":{"cabbage", "carrot"},
+                    "002":{"fruits":{"apple", "mango"}, 
+                    "vegetables":{"cabbage", "carrot"}}}
 class GetRecommendation(Resource):
     def get(self,cust_id):
-        return recommendations[cust_id]
+        return {"name":cust_id}
     
     def post(self):
         return {"data": "grocery recommendations"}
 # route
-api.add_resource(GetRecommendation, "/recommend/<")
+api.add_resource(GetRecommendation, "/recommend/<int:cust_id>")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
--- recommender/test.py
+++ recommender/test.py
@@ -2,5 +2,5 @@ import  requests
 
 BASE = "http://127.0.0.1:8080/"
 
-response = requests.get(BASE + "recommend")
+response = requests.get(BASE + "recommend/cust_id")
 print(response.json())
