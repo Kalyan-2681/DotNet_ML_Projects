10f0c23f1cee1f2c1ce12d0f2db277801207d8dd
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2021-08-26 16:10:12 -0400

    updated code to fetch site information
    updated code to fetch site information to make sure recommender knows what results to send

--- recommender/dboperations.py
+++ recommender/dboperations.py
@@ -3,10 +3,37 @@ import pymongo
 from pymongo import MongoClient
 import bson
 from bson.raw_bson import RawBSONDocument
+import logging
+
+logging.info("inside dboperations")
+
+def getcustomer(cust_id):
+        logging.info("inside getcustomer")
+        results = readfromdb(cust_id)
+        return {"data": results}
+ 
+
+def readfromdb(searchString): 
+    logging.info("inside readfromdb")   
+    USR = "webuser"   
+    PWD = "carnivaL1"
+    DB_NAME = "ElectronicEcommerce"  # Specifiy a Database Name
+    CLUSTER = f"mongodb+srv://{USR}:{PWD}@cluster0.hhuga.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
+    COLLECTION_NAME = "Site"
+    dbclient = MongoClient(CLUSTER)
+    dataBase = dbclient[DB_NAME]
+    collection = dataBase[COLLECTION_NAME]
+    for record in collection.find_all({},{'site_id': searchString}):
+        result = []
+        result = record.__dict__
+        logging.info("records :  " + record.__dict__) 
+    return result       
+
+    dbclient.close()
 
 def writetodb(searchString,searchdata):    
     USR = "webuser"   
-    PWD = "####"
+    PWD = "carnivaL1"
     DB_NAME = "ElectronicEcommerce"  # Specifiy a Database Name
     CLUSTER = f"mongodb+srv://{USR}:{PWD}@cluster0.hhuga.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
     COLLECTION_NAME = "Site"
--- recommender/recommender.py
+++ recommender/recommender.py
@@ -1,21 +1,35 @@
 from flask import Flask
 from flask_restful import Api, Resource
-#import dboperations
+import dboperations
 import logging
 
 app = Flask(__name__)
 api = Api(app)
 
-recommendations = {"001":{"fruits":{"apple", "mango"}, 
-                    "vegetables":{"cabbage", "carrot"},
-                    "002":{"fruits":{"apple", "mango"}, 
-                    "vegetables":{"cabbage", "carrot"}}}
+#dummy data
+recommendations = {"001":{"fruits":{"apple", "mango", "fig", "guava"}, "vegetables":{"cabbage","onion", "garlic", "carrot", "potato"}},
+                   "002":{"fruits":{"apricot", "mango", "plum"}, "vegetables":{"cabbage", "carrot"}},
+                   "003":{"fruits":{"banana", "mango", "nectarine"}, "vegetables":{"cabbage","garlic", "carrot"}},
+                   "004":{"fruits":{"apple", "mango", "grapes"}, "vegetables":{"cabbage", "carrot", "potato"}},
+                   "005":{"fruits":{"blackberry", "mango", "orange", "cherry"}, "vegetables":{"cabbage","onion", "carrot"}}
+                   }
+
+logging.basicConfig(filename='recommender.log', encoding='utf-8', level=logging.DEBUG)
+logging.info("check 1")
 class GetRecommendation(Resource):
     def get(self,cust_id):
-        return {"name":cust_id}
+        try:
+            sitedata = dboperations.getcustomer(cust_id)
+            logging.info("getting site information")
+            return sitedata["site_name"]      
+        except:
+            logging.info("default site information")    
+            return {"Site Name":"Catering"}
     
     def post(self):
-        return {"data": "grocery recommendations"}
+        return {"data": "recommendations"}
+    
+    
 # route
 api.add_resource(GetRecommendation, "/recommend/<int:cust_id>")
 
