71d775904221cb68e8a02d6a2ca2b722ea9daa67
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2021-10-03 11:05:59 -0400

    SQL Lite integration

--- todolistapp/app.py
+++ todolistapp/app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, url_for
+from  flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.models.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __rep__(self):
+        return '<Task %r>' % self.id
 
 
 @app.route('/')
