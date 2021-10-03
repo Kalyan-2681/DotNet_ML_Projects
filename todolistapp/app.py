1d1c6f86a6b1bbb7359f9e2d454f8380cbacaf6d
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2021-10-03 11:44:13 -0400

    added table to index.html

--- todolistapp/app.py
+++ todolistapp/app.py
@@ -1,17 +1,19 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for
 from  flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+db.init_app(app)
 
-class Todo(db.models.Model):
+class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
 
-    def __rep__(self):
+    def __repr__(self):
         return '<Task %r>' % self.id
 
 
