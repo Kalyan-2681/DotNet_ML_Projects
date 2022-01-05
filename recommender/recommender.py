6791a85549a18c813f174fcd6ee509035c550356
mekalyan26 <adhikari.kalyan@gmail.com>
2022-01-05 06:58:59 -0500

    function to get user cart details from session

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -20,21 +20,22 @@ recommendations = {"001":{"fruits":{"apple", "mango", "fig", "guava"}, "vegetabl
 logging.basicConfig(filename='recommender.log', encoding='utf-8', level=logging.DEBUG)
 logging.info("check 1")
 
-"""class GetRecommendation(Resource):
-    def get(self,cust_id):
-        try:
-            sitedata = dboperations.getcustomer(cust_id)
-            logging.info("getting site information")
-            return sitedata["site_name"]      
-        except:
-            logging.info("default site information")    
-            return {"Site Name":"Catering"}
-"""
+def get_items_from_cart(self,cust_id):
+    """ Fetches items from sessions cart"""
+    item_list = []
+    cart_count = self.session.get('add_to_cart_count')
+    if not cart_count: return None;
+    for i in range(1, cart_count+1):
+        item = self.session.get(str(i))
+        if item:
+            item_list.append(item)
+    return item_list
 
 @app.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
-    return render_template("index.html")   
+    return render_template("index.html") 
+
 
 @app.route('/recommend/<int:cust_id>',methods=['POST','GET']) # route to show the predictions in UI
 @cross_origin()
@@ -43,6 +44,9 @@ def index():
         try:
             #  reading the customer id who has logged in to the online grocery store
             cust_id = request.args.get('cust_id')
+
+            # retrieving customer cart details from session
+            item_list = get_items_from_cart(cust_id)
             
             if(cust_id!= None):
                 research=1
@@ -50,8 +54,13 @@ def index():
                 research=0
             filename = "recommenderModel_KNN.pkl"
             loaded_model = pickle.load(open(filename, 'rb')) # loading the model file from the storage
-            # predictions using the loaded model file
+
+            # User based recommend predictions using the loaded KNN model
             prediction=loaded_model.predict([[cust_id]])
+
+            # Item based recommend predictions using the loaded KNN model
+            #prediction=loaded_model.predict([[item_list]])
+
             print('prediction is :', prediction)
             # showing the prediction results in a UI
             return render_template('results.html',prediction=round(100*prediction[0]))
