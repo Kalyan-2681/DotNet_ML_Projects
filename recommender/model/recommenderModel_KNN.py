4be4c1088171900d0335ebaa4683fa4f6d48d1c7
mekalyan26 <adhikari.kalyan@gmail.com>
2022-01-12 07:53:19 -0500

    added algorithm

--- recommender/model/recommenderModel_KNN.py
+++ recommender/model/recommenderModel_KNN.py
@@ -9,6 +9,8 @@ import json
 import os
 from itertools import groupby
 from logger import logger
+from sklearn.decomposition import TruncatedSVD
+from sklearn.neighbors import NearestNeighbors
 
 
 class RecommenderModel:
@@ -16,7 +18,7 @@ class RecommenderModel:
         self.file_object = './logs/recommendationModelKNN.log'        
         self.log_writer = logger.logger(self.file_object)
 
-    def getDatafromDB(COLLECTION_NAME):
+    def getDatafromDB(Collectionname):
         # User name
         USR = "webuser" # readWrite for DB
         # PWDPLAINTXT = base64.b64encode("password".encode("utf-8"))
@@ -25,7 +27,7 @@ class RecommenderModel:
         PWD = "password"
         DB_NAME = "ElectronicCommerce"  # Specifiy a Database Name
         CLUSTER = f"mongodb+srv://{USR}:{PWD}@cluster0.hhuga.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
-        COLLECTION_NAME = "Customer"
+        COLLECTION_NAME = Collectionname
         dbclient = MongoClient(CLUSTER)
         dataBase = dbclient[DB_NAME]
         collection = dataBase[COLLECTION_NAME]
@@ -38,4 +40,18 @@ class RecommenderModel:
         dbclient.close()
         return df
 
+    def predictKNN(pivoted_data):
         
+        svd = TruncatedSVD(n_components=200)
+        latent_matrix = svd.fit_transform(pivoted_data)
+        model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
+        model_knn.fit(pivoted_data) 
+
+        query_index = np.random.choice(pivoted_data.shape[0])        
+        distances, indices = model_knn.kneighbors(pivoted_data.iloc[query_index,:].values.reshape(1, -1), n_neighbors = 6)
+
+        for i in range(0, len(distances.flatten())):
+            if i == 0:
+                return (pivoted_data.index[query_index])
+            else:
+                return (i, pivoted_data.index[indices.flatten()[i]], distances.flatten()[i])
