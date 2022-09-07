7a1826f555e6e0fa4b09a89dc583d729a375d7bb
Kalyan Adhikari <adhikari.kalyan@gmail.com>
2022-09-06 21:19:31 -0400

    updated code to add arduino digital pin activation
    updated code to add arduino digital pin activation for led glow

--- facedetection/README.md
+++ facedetection/README.md
@@ -1,7 +1,8 @@
 Python project 
 1. Face Detection
 2. Send instructions to computer serial port
-3. Based on instructions from serial port , arduino's pins are activated
+3. Based on instructions from serial port , arduino's digital pins 12 and 13 are activated 
+
 
 Python Libraries:
 - cvzone
--- facedetection/read-2560.py
+++ facedetection/read-2560.py
@@ -2,10 +2,13 @@ from sre_constants import SUCCESS
 import cv2
 from cvzone.FaceDetectionModule import FaceDetector
 from cvzone.SerialModule import SerialObject
+#from serial import Serial
 
 cap = cv2.VideoCapture(0)
 detector = FaceDetector()
-arduino = SerialObject('/dev/ttyACM0')
+#arduino = SerialObject('/dev/ttyACM0')
+
+arduino2 = SerialObject(portNo='/dev/ttyACM0', baudRate=115200)
 
 while True:
     SUCCESS, img = cap.read()
@@ -13,9 +16,10 @@ while True:
     img, bBoxes = detector.findFaces(img)
 
     if bBoxes:
-        arduino.sendData([1,0])
+        arduino2.sendData([1,0])
     else:
-        arduino.sendData([0,1])
+        arduino2.sendData([0,1])
+    
     cv2.imshow("Video", img)
     cv2.waitKey(1)
 
